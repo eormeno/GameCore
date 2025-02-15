c7b6bcc9e43aa38cccc57fe7b087ec4660b32990
Emilio Ormeño <eormneno@gmail.com>
2025-02-15 17:28:02 -0300

    update index.html for Spanish localization and add game card display; implement image serving route

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,13 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return response()->file(public_path('index.html'));
 });
+
+Route::get('/storage/images/{filename}', function ($filename) {
+    $path = storage_path('app/public/images/' . $filename);
+
+    if (!file_exists($path)) {
+        abort(404);
+    }
+
+    return response()->file($path);
+});
