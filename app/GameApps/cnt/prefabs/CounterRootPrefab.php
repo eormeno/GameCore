e73e72e1deb332c9ee6e81d75938798444d3afdc
Emilio Ormeño <eormneno@gmail.com>
2025-03-01 22:07:21 -0300

    refactor CounterRootPrefab attributes to streamline layout configuration

--- app/GameApps/cnt/prefabs/CounterRootPrefab.php
+++ app/GameApps/cnt/prefabs/CounterRootPrefab.php
@@ -11,12 +11,7 @@ class CounterRootPrefab extends Prefab
     {
         return [
             'main:container' => [
-                'attributes' => [
-                    'layout' => 'horizontal',
-                    'image' => 'counter_background.png',
-                    'width' => '100%',
-                    'height' => '100%'
-                ],
+                'attributes' => ['layout' => 'horizontal', 'width' => '100%', 'height' => '100%'],
                 'dec_button:button' => ['attributes' => ['text' => '-', 'event' => 'decrement', 'style' => 'primary']],
                 'number:label' => ['attributes' => ['text' => '0', 'style' => 'label-center']],
                 'inc_button:button' => ['attributes' => ['text' => '+', 'event' => 'increment', 'style' => 'primary']],
