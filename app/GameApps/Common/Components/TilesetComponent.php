d3aeedc7f405011de9b7fe0ac585a27fcf49fef2
DAMO\Emilio <eormeno@gmail.com>
2025-03-07 09:24:43 -0300

    refactor TilesetComponent to include default number and image; update game configurations to set active state to false for BBA, CNT, and MTQ games

--- app/GameApps/Common/Components/TilesetComponent.php
+++ app/GameApps/Common/Components/TilesetComponent.php
@@ -6,33 +6,38 @@ use App\Models\Components\PersistentComponent;
 
 class TilesetComponent extends PersistentComponent
 {
+    private const DEFAULT_NUMBER = 0;
     private const TILE_WIDTH = 128;
     private const TILE_HEIGHT = 128;
 
-	public static function config(): array
-	{
-		return [
-			'tile_width' => ['integer', self::TILE_WIDTH],
+    public static function config(): array
+    {
+        return [
+            'number' => ['integer', self::DEFAULT_NUMBER],
+            'tile_width' => ['integer', self::TILE_WIDTH],
             'tile_height' => ['integer', self::TILE_HEIGHT],
-			'tilemap' => ['string', null],
-		];
-	}
+            'image' => ['string', null],
+        ];
+    }
 
-	public function onAwake(array $initParams): void
-	{
+    public function onAwake(array $initParams): void
+    {
         $this->updateQuietly([
+            'number' => $initParams['number'] ?? self::DEFAULT_NUMBER,
             'tile_width' => $initParams['tile_width'] ?? self::TILE_WIDTH,
             'tile_height' => $initParams['tile_height'] ?? self::TILE_HEIGHT,
-            'tilemap' => $initParams['tilemap'] ?? null,
+            'image' => $initParams['image'] ?? null,
         ]);
     }
 
-	public function view()
-	{
-        return view('tileset', [
+    public function view()
+    {
+        return [
+            'type' => 'tileset',
+            'number' => $this->number,
             'tile_width' => $this->tile_width,
             'tile_height' => $this->tile_height,
-            'tilemap' => $this->tilemap,
-        ]);
+            'image' => $this->image,
+        ];
     }
 }
--- app/GameApps/rpg/prefabs/RpgRootPrefab.php
+++ app/GameApps/rpg/prefabs/RpgRootPrefab.php
@@ -10,6 +10,15 @@ class RpgRootPrefab extends Prefab
     public static function structure(): array
     {
         return [
+            'land:tileset' => [
+                'attributes' => ['number' => 0, 'tile_width' => 128, 'tile_height' => 128, 'image' => 'land.png'],
+            ],
+            'water:tileset' => [
+                'attributes' => ['number' => 1,'tile_width' => 128, 'tile_height' => 128, 'image' => 'water.png'],
+            ],
+            'building:tileset' => [
+                'attributes' => ['number' => 2,'tile_width' => 128, 'tile_height' => 128, 'image' => 'building.png'],
+            ],
             'main:container' => [
                 'attributes' => ['layout' => 'horizontal', 'width' => '100%', 'height' => '100%'],
                 'dec_button:button' => ['attributes' => ['text' => '-', 'event' => 'decrement', 'style' => 'primary']],
