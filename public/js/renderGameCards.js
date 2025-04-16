eeb557bde7898a87591bd6d848360936c70012aa
eormeno <eormeno@gmail.com>
2025-04-16 08:31:15 -0300

    refactor game card rendering to remove commented-out code; streamline login form script to utilize container parameters for page state management

--- public/js/renderGameCards.js
+++ public/js/renderGameCards.js
@@ -8,38 +8,6 @@ async function renderGamesCards(games) {
     for (let game of games) {
         const card = document.createElement('div');
 
-        // const image = document.createElement('img');
-        // image.className = 'game-image';
-        // image.src = 'api/game-app/' + game.id + '/public/' + game.card_image;
-        // image.alt = game.name;
-
-        // const content = document.createElement('div');
-        // content.className = 'game-content';
-
-        // const title = document.createElement('h2');
-        // title.className = 'game-title';
-        // title.textContent = game.name;
-
-        // const description = document.createElement('p');
-        // description.className = 'game-description';
-        // description.textContent = game.description;
-
-        // const playButton = document.createElement('button');
-        // playButton.className = 'play-button';
-        // playButton.textContent = 'Jugar';
-        // playButton.disabled = game.prefab_name === null;
-        // playButton.onclick = () => pageState.setPageState('fetching_game', { id: game.id });
-
-        // content.appendChild(title);
-        // content.appendChild(description);
-        // content.appendChild(playButton);
-
-        // card.appendChild(image);
-        // card.appendChild(content);
-
-        // container.appendChild(card);
-        // console.log('game', game);
-
         await partialLoader.loadPartial('game-card', card, {
             game: game,
             pageState: pageState
