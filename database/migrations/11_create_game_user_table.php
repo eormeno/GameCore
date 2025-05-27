b9a775eb626f4bb6b30c59e22ef71b86fcb896a1
Emilio Ormeño <eormeno@iinfo.unsj.edu.ar>
2025-05-27 10:05:59 -0300

    feat: Add ownership and access control fields to game_user migration

--- database/migrations/11_create_game_user_table.php
+++ database/migrations/11_create_game_user_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -15,6 +14,9 @@ return new class extends Migration
             $table->id();
             $table->foreignId('game_id')->constrained();
             $table->foreignId('user_id')->constrained();
+            $table->boolean('is_owner')->default(false);
+            $table->boolean('access_approved')->default(false);
+            $table->boolean('invitation_accepted')->default(false);
             $table->timestamps();
         });
     }
