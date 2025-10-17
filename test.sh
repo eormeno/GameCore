8724d832a8613ad0fdbdb63dafeb43c976dba76c
Tu Nombre <eormeno@gmail.com>
2025-10-17 14:09:48 -0300

    feat: Add UIBuilderTest to test class mapping for improved test filtering

--- test.sh
+++ test.sh
@@ -18,6 +18,7 @@ declare -A testClasses=(
     ["gtn"]="GTNPlayGameTest"
     ["bba"]="BBAPlayGameTest"
     ["mtq"]="MTQPlayGameTest"
+    ["ui"]="UIBuilderTest"
 )
 
 # Find the filter in prefix and get the test class name
