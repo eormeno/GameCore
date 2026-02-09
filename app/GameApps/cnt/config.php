db68f80359fc62d8c2510513d6969c367ad3b1a6
Emilio Ormeño <eormeno@iinfo.unsj.edu.ar>
2026-02-09 12:47:38 -0300

    fix: Update max_instances_per_user to limit open saved games to 1

--- app/GameApps/cnt/config.php
+++ app/GameApps/cnt/config.php
@@ -2,7 +2,7 @@
 
 return [
     'name' => 'Counter',
-    'max_instances_per_user' => 10, // The maximum number of open saved games a user can have.
+    'max_instances_per_user' => 1, // The maximum number of open saved games a user can have.
     'description' => 'Un juego de interacción con un contador.',
     'card_image' => 'counter-card.png',
     'prefab_name' => 'cnt.counter-root-prefab',
